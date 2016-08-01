121afddf9387cac641a32d1d573d7d41d79f705c
Michael <michael.krohn@colorado.edu>
2016-07-31 19:11:26 -0500

    First commit

---
+++ 76X_JetsHT.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21/JetHT/VHBB_HEPPY_V21_JetHT__Run2015C_25ns-16Dec2015-v1/160318_132855/0000/ --outName=Jet_HT_C --trigger=False --jets=True --deta=True --min=0 --max=31 --file=TxtFiles/76XRunC.txt &
+./cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc530
+scramv1 project CMSSW CMSSW_8_0_12
+cd CMSSW_8_0_12/src
+eval `scramv1 runtime -sh`
+cp ../../generalTreeAnalyzer_76X_JetsHT.py .
+cp ../../JetHT_Skimmed.txt .
+cp ../../trigger_objects.root .
+
+python generalTreeAnalyzer_76X_JetsHT.py --pathIn=root://cmsxrootd.fnal.gov//store/user/mkrohn/HHTo4b/SkimmedHeppy_nTuples/ --outName=JetsHT --trigger=False --jets=True --deta=True --min=0 --max=1 --file=JetHT_Skimmed.txt
+
+xrdcp JetsHT_0.root root://cmseos.fnal.gov//store/user/mkrohn/HHTo4b/SkimmedHeppy_nTuples/miniTrees_pt100/
